22fe1d2b59d702b49c3e0500377083e62ad410fc
Brookes Stephens <broox9@gmail.com>
2014-12-05 12:18:56 -0500

    ngTouch implemented and working

--- public/js/app/controllers/data_controller.js
+++ public/js/app/controllers/data_controller.js
@@ -1,62 +1,17 @@
-ou3.controller('DataController', ['$scope', 'uiGmapGoogleMapApi', function ($scope, uiGmapGoogleMapApi) {
-  $scope.name = "St Francis Hospital";
+ou3.controller('DataController', ['$scope','placer', 'uiGmapGoogleMapApi', function ($scope, placer, uiGmapGoogleMapApi) {
   $scope.mapLoaded = false;
+  $scope.area = {};
 
-  $scope.area = {
-    hood: "",
-    city: "",
-    zip: "",
-    street: "",
-    county: "",
-    state: ""
-  };
-
-  $scope.locationData = [];
-
-  function typeGrep (type) {
-    var info = $.grep($scope.locationData, function (item) {
-      return item.types[0] == type;
-    });
-
-    console.log("grepped info", type, info)
-    try {
-      return info[0]['long_name']
-    } catch (error) {
-      return ''
-    }
-  }
-
-//  function localityGrep(typeArr) {
-//    var txt = '';
-//
-//    for (var i = 0, n = typeArr.length; i < n; i++) {
-//      var text = $.grep($scope.locationData, function (item) {
-//        try {
-//          return item.types[0] == type;
-//        } catch(err) {
-//          return '';
-//        }
-//      });
-//      if (text.length) {
-//        txt = text;
-//      }
-//    }
-//
-//    return txt;
-//  }
+  $scope.swiped = false
 
   $scope.$on('geocode', function (e, data) {
-    console.log("rec'd geocode", data)
     $scope.locationData = data;
-
-    $scope.area.hood = typeGrep('neighborhood');
-    $scope.area.city = typeGrep('locality') || '';
-    $scope.area.zip = typeGrep('postal_code');
-    $scope.area.county = typeGrep('administrative_area_level_2');
-    $scope.area.street = typeGrep('route');
-    $scope.area.state = typeGrep('administrative_area_level_1');
-    console.log("city",$scope.area.hood);
+    $scope.area = placer.extract(data);
   });
+
+  $scope.handleSwipe = function (e) {
+    console.log("swipe", e)
+  };
 
 
   //once the map API is loaded..
--- public/js/app/ou3.js
+++ public/js/app/ou3.js
@@ -1,4 +1,4 @@
-var ou3 = angular.module("ou3", ['uiGmapgoogle-maps'])
+var ou3 = angular.module("ou3", ['uiGmapgoogle-maps', 'ngTouch'])
     .config(['uiGmapGoogleMapApiProvider', function(uiGmapGoogleMapApiProvider) {
       uiGmapGoogleMapApiProvider.configure({
         //    key: 'your api key',
@@ -7,14 +7,46 @@ var ou3 = angular.module("ou3", ['uiGmapgoogle-maps'])
     });
 }]);
 
-ou3.factory('geo', ['$window', function($window) {
-  return  {
-    getLocation: $window.navigator.geolocation.getCurrentPosition
+
+ou3.factory('placer', [function () {
+
+  var extract = function (data) {
+    // *usually* aa1 = state aa2= county  sl1 = borough
+    level_codes = [
+      'neighborhood',
+      'route',
+      'postal_code',
+      'postal_code_suffix',
+      'locality',
+      'sublocality_level_1',
+      'administrative_area_level_1',
+      'administrative_area_level_2'
+    ];
+
+    console.log("raw data", data)
+    return typeGrep(data,level_codes);
+
   };
 
-}]);
+  function typeGrep (data,level_codes) {
+    //need to figure out how to find what failed
+    var extractObj =  {};
 
+    for(var i = 0, n = level_codes.length; i < n; i++) {
+      var item = $.grep(data, function (d, idx) {
+        return d.types[0] == level_codes[i]
+      });
+      if (item.length) {
+        extractObj[level_codes[i]] = item[0]['long_name']
+      }
+    }
 
-ou3.factory('places', ['uiGmapGoogleMapApiProvider', function (uiGmapGoogleMapApiProvider) {
+    console.log("extracted Data", extractObj);
+    return extractObj;
+  }
+
+  return {
+    extract: extract
+  }
 
 }]);
