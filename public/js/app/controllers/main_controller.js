0fd8251a3edc7ffe70018244bb56ba880ea5633d
Brookes Stephens <broox9@gmail.com>
2014-12-18 15:05:07 -0500

    rolled my own maps thank you very much

--- public/js/app/controllers/main_controller.js
+++ public/js/app/controllers/main_controller.js
@@ -1,84 +1,40 @@
 ou3.controller('MainController', [
   '$scope',
   '$window',
-  'uiGmapGoogleMapApi',
-  function ($scope, $window, uiGmapGoogleMapApi) {
-    //default to St. Francis Hospital ... where I started **rimshot**
-    $scope.position = {latitude: 40.216339, longitude: -74.741427};
-
-    $scope.marker = {
-      idKey: Date.now(),
-      coords: {
-        latitude: $scope.position.latitude,
-        longitude: $scope.position.longitude
-      },
-      map: {} //$scope.map.controlOpts.getGMap()
+  '$document',
+  'brooxMap',
+  function ($scope, $window, $document, brooxMap) {
+    //initial check
+    if (!$window.navigator.geolocation) {
+      alert("This isn't gonna work out :/");
     }
+    //set scope vars
+    $scope.geocoder = new brooxMap.google.maps.Geocoder();
+    $scope.mapCanvas = document.querySelector('#map-canvas');
+    $scope.position = {lat:40.216339, lng: -74.741427};  //default coords
+
 
     $scope.updatePosition = function () {
-      $window.navigator.geolocation.getCurrentPosition(updateLocation, showLocationError);
-    }
-
-    $scope.map = {
-      center: $scope.position,
-      zoom: 15,
-      disableDefaultUI: true,
-      streetViewControl: true,
-      scaleControl: false,
-      mapTypeControl: false,
-      refresh: false,
-      controlOpts: {} //gets methods added by the whatchamacallit. custom methods can be added
+      $window.navigator.geolocation.getCurrentPosition(function (location) {
+        $scope.position = new brooxMap.LatLng(location.coords.latitude, location.coords.longitude);
+        brooxMap.updateMap($scope.position);
+        console.log("location")
+        $scope.geocoder.geocode({'latLng': $scope.position}, handleGeoCode);
+      })
     };
 
+    $scope.updatePosition();
 
-    function updateLocation (location) {
-      $scope.position = new google.maps.LatLng(location.coords.latitude, location.coords.longitude);
-      $scope.geocoder.geocode({'latLng': $scope.position}, handleGeoCode);
 
-      try {
-        $scope.map.controlOpts.refresh(location.coords);
-        setMarker(location)
-      } catch (error) {
-        console.log("there is an error with refresh")
-      }
-    }
-
-    function setMarker (loc) {
-//      console.log("setting marker")
-//      var myLatLng = new $scope.map_service.LatLng(loc.coords.latitude, loc.coords.longitude)
-//      var map = $scope.map.controlOpts.getGMap();
-//
-//      var marker = new $scope.map_service.maps.Marker({
-//        position: myLatLng,
-//        map: map,
-//        title: 'Hello World!'
-//      });
-//
-//      console.log('Marker', marker, myLatLng)
-
-    }
-
-    function showLocationError (error) {
-      console.log("Loc Error, loc", error)
-      return $scope.position;
-    }
+    //make sure google service is loaded
+    brooxMap.loadGoogle.then(function () {
+      $scope.map = brooxMap.initMap($scope.mapCanvas);
+    });
 
     function handleGeoCode (data) {
       if (data.length && data[0]['address_components']) {
         $scope.$broadcast('geocode', data[0]['address_components'])
       }
     }
-
-    //once the map API is loaded..
-    uiGmapGoogleMapApi.then(function (maps) {
-      $scope.map_service = maps;
-      $scope.geocoder = new maps.Geocoder();
-
-      $scope.map.refresh = true;
-      //return maps;
-    }).then(function (maps) {
-      //console.log("Next promise", maps);
-      $window.navigator.geolocation.getCurrentPosition(updateLocation, showLocationError);
-    });
 
 }])
--- public/js/app/ou3.js
+++ public/js/app/ou3.js
@@ -1,10 +1,13 @@
 var ou3 = angular.module("ou3", ['uiGmapgoogle-maps', 'ngTouch'])
-    .config(['uiGmapGoogleMapApiProvider', function(uiGmapGoogleMapApiProvider) {
-      uiGmapGoogleMapApiProvider.configure({
-        //    key: 'your api key',
-        v: '3.17',
-        libraries: 'weather,geometry,visualization,places'
-    });
+  .constant('api_keys',{
+    'google_maps': 'AIzaSyD1KY0AYxsv3GjDFZRrfTQA3MpFAr4zZtU'
+  })
+  .config(['uiGmapGoogleMapApiProvider', function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+      //    key: 'your api key',
+      v: '3.17',
+      libraries: 'weather,geometry,visualization,places'
+  });
 }]);
 
 
@@ -47,6 +50,76 @@ ou3.factory('placer', [function () {
 
   return {
     extract: extract
+  }
+
+}]);
+
+
+ou3.factory('brooxMap', ['$q','$window', function ($q, $window) {
+  var google = $window.google;
+  var loadGoogleDefer = $q.defer();
+  var coordinates = {lat: 40.216339, lng: -74.741427};
+  var map = null;
+  var currentMarker = null;
+
+  google.maps.event.addDomListener($window, 'load', googleLoaded);
+
+  function googleLoaded () {
+    loadGoogleDefer.resolve(true);
+  }
+
+  function initMap (canvas) {
+    var mapOptions = {
+      center: coordinates, //new $g.maps.LatLng($scope.position.latitude, $scope.position.longitude),
+      zoom: 15,
+      disableDefaultUI: true,
+      mapTypeControl: true,
+      mapTypeControlOptions: {
+        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU
+      },
+      streetViewControl:true
+    };
+
+    map = new google.maps.Map(canvas, mapOptions);
+    setMarker();
+    return map;
+  }
+
+  function updateMap (coords) {
+    if (coordinates.hasOwnProperty('k')) {
+      coordinates = new google.maps.LatLng(coords.latitude, coords.longitude)
+    } else {
+      coordinates = coords
+    }
+
+
+    if(map) {
+      map.setCenter(coordinates);
+      setMarker();
+    }
+  };
+
+  function setMarker () {
+    if (currentMarker) {
+      currentMarker.setMap(null)
+    }
+
+    currentMarker = new google.maps.Marker({
+      position: coordinates,
+      map: map,
+      animation: google.maps.Animation.DROP,
+      title: "You are Here"
+    })
+  }
+
+  return {
+    loadGoogle: loadGoogleDefer.promise,
+    google: google,
+    initMap: initMap,
+    updateMap: updateMap,
+    LatLng: google.maps.LatLng,
+    getMarker: function () { return currentMarker},
+    getMap: function () { return map}
   }
 
 }]);
